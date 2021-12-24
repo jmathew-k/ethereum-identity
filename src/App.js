3e27a8a303cd4b25105137fd82c3c8431ed067ca
Joshua Mathew <JoshuaKMathew@gmail.com>
2021-12-24 19:52:01 +0530

    iMac - unfinished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+
+import './App.css'
+import { useState } from 'react'
+
+import CeramicClient from '@ceramicnetwork/http-client'
+import ThreeIdResolver from '@ceramicnetwork/3id-did-resolver'
+
+import { EthereumAuthProvider, ThreeIdConnect } from '@3id/connect'
+import { DID } from 'dids'
+import { IDX } from '@ceramicstudio/idx'
+
+const endpoint = "https://cermaic-clay.3boxlabs.com"
 
 function App() {
+  const [name, setName] = useState('')
+  const [image, setImage] = useState('')
+  const [loaded, setLoaded] = useState(false)
+
+  async function connect() {
+    const addresses = await window.ethereum.request({
+      method: 'eth_requestAccounts'
+    })
+      return addresses
+  }
+
+async function readProfile() {
+  const [address] = await connect()
+  const ceramic = new CeramicClient(endpoint)
+  const idx = new IDX({ ceramic })
+
+
+  try {
+    const data = await idx.get(
+      'basicProfile',
+      `${address}@eip115:1`
+    )
+    console.log('data: ', data)
+    if (data.name) setName(data.name)
+    if (data.avatar) setImage(data.avatar)
+  } catch (error) {
+    console.log('error: ', error)
+    setLoaded(true)
+  }
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
