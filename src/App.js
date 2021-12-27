4dbdadeee0841fcc6d81f82131e0180dcb096d6f
Joshua Mathew <JoshuaKMathew@gmail.com>
2021-12-27 20:08:13 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -45,12 +45,49 @@ async function readProfile() {
   }
 }
 
+async function updateProfile() {
+	const [address] = await connect()
+	const ceramic = new CeramicClient(endpoint)
+	const threeIdConnect = new ThreeIdConnect()
+	const provider = new EthereumAuthProvider(window.ethereum, address)
 
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	await threeIdConnect.connect(provider)
+
+	const did = new DID({
+		provider: threeIdConnect.getDidProvider(),
+		resolver: {
+			...ThreeIdResolver.getResolver(ceramic),
+		},
+	})
+
+	ceramic.setDID(did)
+	await ceramic.did.authenticate()
+
+	const idx = new IDX({ ceramic })
+
+	await idx.set('basicProfile', {
+		name,
+		avatar: image,
+	})
+
+	console.log('Profile updated!')
+}
+
+return (
+	<div className='App'>
+		<input placeholder='Name' onChange={(e) => setName(e.target.value)} />
+		<input
+			placeholder='Profile Image'
+			onChange={(e) => setImage(e.target.value)}
+		/>
+		<button onClick={updateProfile}>Set Profile</button>
+		<button onClick={readProfile}>Read Profile</button>
+
+		{name && <h3>{name}</h3>}
+		{image && <img style={{ width: '400px' }} src={image} />}
+		{!image && !name && loaded && <h4>No profile, please create one...</h4>}
+	</div>
+)
 }
 
 export default App;
